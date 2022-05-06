4850ac3d630ada4a9bb031adc85a818044f4ebd2
Sirishakopparaju <sirishakopparaju98@gmail.com>
2022-05-06 17:32:35 +0530

    Refactor:used alter for adding type

--- Address.sql
+++ Address.sql
@@ -31,3 +31,13 @@ SELECT * from addressbook where city = 'HYD' OR state = 'TS';
 SELECT  city, state, COUNT(*) from addressbook where city = 'HYD' AND state = 'Telangana';
 SELECT * FROM addressbook WHERE state = 'Telangana' ORDER BY firstname ASC;
 SELECT * FROM addressbook WHERE state = 'Telangana' ORDER BY firstname DESC;
+ALTER  TABLE addressbook add ab_name varchar(20);
+alter table addressbook add ab_type char(20);
+
+update addressbook set ab_name = 'AddressBook1' where id = 2;
+update addressbook set ab_name = 'AddressBook2' where id = 1;
+update addressbook set ab_name = 'AddressBook3' where id IN (3,4);
+
+update addressbook set ab_type = 'family' where id = 1;
+update addressbook set ab_type = 'friends' where id = 2;
+update addressbook set ab_type = 'profession' where id IN(3,4);
